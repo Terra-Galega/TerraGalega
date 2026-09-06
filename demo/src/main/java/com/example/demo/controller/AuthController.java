7f36fbb06dda864097b8423359df4bdc2655ddc7
sophieMjs <sophie.mejia.sc@gmail.com>
2026-09-06 12:47:35 -0500

    Spring 4 sin manejo de errores

--- demo/src/main/java/com/example/demo/controller/AuthController.java
+++ demo/src/main/java/com/example/demo/controller/AuthController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class AuthController {
@@ -129,7 +130,7 @@ public class AuthController {
         }
 
         model.addAttribute("client", Client);
-        return "account";
+        return "/account";
     }
 
 }
