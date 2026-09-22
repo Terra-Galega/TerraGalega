b93759ab809d9e412b66fa22b54422c07d535a9a
Santiago Ibañez <santiagoibanez2580@gmail.com>
2026-09-22 18:49:31 -0500

    Addons without null category ids

--- demo/src/main/java/com/example/demo/DataLoader.java
+++ demo/src/main/java/com/example/demo/DataLoader.java
@@ -8,7 +8,6 @@ import com.example.demo.repository.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import java.util.List;
 import java.util.Random;
 import java.time.LocalDateTime;
 
@@ -128,23 +127,6 @@ public class DataLoader implements CommandLineRunner {
                                 Admin.builder().name("Javier").lastName("Ortiz").email("javier.ortiz@terra.com")
                                                 .password("javier123").role(Role.ADMIN).build());
 
-                // AddOns
-                addonRepository.save(AddOn.builder().name("Ensalada verde")
-                                .description("Adicional para Empanada").price(5000.0).Active(true).build());
-                addonRepository.save(AddOn.builder().name("Extra ajo")
-                                .description("Adicional para mariscos").price(2000.0).Active(true).build());
-                addonRepository.save(AddOn.builder().name("Salsa de limón")
-                                .description("Adicional para mariscos").price(3000.0).Active(true).build());
-
-                addonRepository.save(AddOn.builder().name("Pan de millo")
-                                .description("Adicional para Lacón").price(3500.0).Active(true).build());
-                addonRepository.save(AddOn.builder().name("Cachelos")
-                                .description("Adicional para Lacón").price(6000.0).Active(true).build());
-
-                addonRepository.save(AddOn.builder().name("Nata montada")
-                                .description("Adicional para Tarta de Santiago").price(3000.0).Active(true).build());
-                addonRepository.save(AddOn.builder().name("Helado de vainilla")
-                                .description("Adicional para Tarta de Santiago").price(4000.0).Active(true).build());
 
                 // Categories
                 categoryRepository.save(Category.builder().name("Entradas")
@@ -173,7 +155,24 @@ public class DataLoader implements CommandLineRunner {
                 Category meats = categoryRepository.findByName("Carnes");
                 Category desserts = categoryRepository.findByName("Postres");
 
-                // adicionales para entradas
+                // AddOns
+                addonRepository.save(AddOn.builder().name("Ensalada verde")
+                                .description("Adicional para Empanada").price(5000.0).Active(true).category(starters).build());
+                addonRepository.save(AddOn.builder().name("Extra ajo")
+                                .description("Adicional para mariscos").price(2000.0).Active(true).category(seafood).build());
+                addonRepository.save(AddOn.builder().name("Salsa de limón")
+                                .description("Adicional para mariscos").price(3000.0).Active(true).category(seafood).build());
+
+                addonRepository.save(AddOn.builder().name("Pan de millo")
+                                .description("Adicional para Lacón").price(3500.0).Active(true).category(meats).build());
+                addonRepository.save(AddOn.builder().name("Cachelos")
+                                .description("Adicional para Lacón").price(6000.0).Active(true).category(meats).build());
+
+                addonRepository.save(AddOn.builder().name("Nata montada")
+                                .description("Adicional para Tarta de Santiago").price(3000.0).Active(true).category(desserts).build());
+                addonRepository.save(AddOn.builder().name("Helado de vainilla")
+                                .description("Adicional para Tarta de Santiago").price(4000.0).Active(true).category(desserts).build());
+
                 addonRepository.save(AddOn.builder().name("Porción de Pan Rústico")
                                 .description("Pan gallego artesanal, ideal para acompañar tus entradas y mojar salsas.")
                                 .price(3500.0).Active(true).category(starters).build());
